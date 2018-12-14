68fb140b499c70bee5b8b21e245b7414e42bfed9
Daniel Schaefer <git@danielschaefer.me>
2018-12-14 21:36:29 +0100

    multivault: init at 2018-09-19

--- default.nix
+++ default.nix
@@ -8,6 +8,10 @@ rec {
     pkgs.python3Packages.callPackage ./pkgs/python-pkgs { }
   );
 
+  multivault = pkgs.callPackage ./pkgs/multivault.nix {
+    myPython3Packages = python3Packages;
+  };
+
   rederr = pkgs.callPackage ./pkgs/rederr.nix {};
 
   python-oath = pkgs.callPackage ./pkgs/python-oath.nix {};
