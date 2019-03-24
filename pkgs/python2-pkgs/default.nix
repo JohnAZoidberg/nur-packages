5463a0e66d02b37fb2b89b43a7814121d41d7a71
Daniel Schaefer <git@danielschaefer.me>
2019-03-24 14:26:51 +0100

    tavern: init at 0.26.1

--- pkgs/python2-pkgs/default.nix
+++ pkgs/python2-pkgs/default.nix
@@ -1,5 +1,6 @@
 { callPackage }:
 rec {
+  tavern = callPackage ./tavern.nix { inherit pykwalify python-box paho-mqtt; };
   pykwalify = callPackage ./pykwalify.nix {};
   python-box = callPackage ./python-box.nix {};
   paho-mqtt = callPackage ./paho-mqtt.nix {};
